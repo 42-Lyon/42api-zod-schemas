e1c0936424856bae705c74064a1621c92abde052
Ian BERTRAND <cameo@42lyon.fr>
2026-02-18 22:42:22 +0100

    fix: cursus kind enum to object

--- src/resources/cursus/enums/kind.ts
+++ src/resources/cursus/enums/kind.ts
@@ -1,13 +1,15 @@
 import z from "zod";
 
-export enum IntraCursusKind {
-	External = "external",
-	Main = "main",
-	MainDeprecated = "main_deprecated",
-	Piscine = "piscine",
-	PiscineCommunity = "piscine_community",
-	PiscineDeprecated = "piscine_deprecated",
-	ProfessionalTraining = "professional_training",
-}
+export const IntraCursusKind = {
+	External: "external",
+	Main: "main",
+	MainDeprecated: "main_deprecated",
+	Piscine: "piscine",
+	PiscineCommunity: "piscine_community",
+	PiscineDeprecated: "piscine_deprecated",
+	ProfessionalTraining: "professional_training",
+} as const;
 
 export const intraCursusKindSchema = z.enum(IntraCursusKind);
+
+export type IntraCursusKind = z.infer<typeof intraCursusKindSchema>;
